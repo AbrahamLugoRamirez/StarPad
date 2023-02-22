0131c402a27c0c05361a1a4c4e8990fe9483999b
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2023-02-22 12:25:56 -0500

    feat: deploy starPad test in netlify.app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'star-pad';
   isPlay = false;
 
   G = new Audio('../assets/G.mp3');
+  // G_ = new Audio(
+  //   "https://github.com/AbrahamLugoRamirez/StarPad/blob/gh-pages/assets/G.mp3"
+  // );
 
-  play(){
-    if(!this.isPlay){
-      this.G.play();
-      this.isPlay = true;
-    }else{
-      this.G.pause()
+  async play() {
+    if (!this.isPlay) {
+      console.log(this.G)
+      const promise = this.G;
+      await this.playSound(promise);
+    } else {
+      this.G.pause();
+    }
+    this.isPlay = !this.isPlay;
+  }
+
+  async playSound(promise: any) {
+    console.log('Promise: ', promise);
+    if (promise) {
+      await promise.play()
+        .then(function () {
+          // Automatic playback started!
+          console.log('Playing...');
+        })
+        .catch(function (error: any) {
+          console.error('error play', error);
+          // Automatic playback failed.
+          // Show a UI element to let the user manually start playback.
+        });
     }
   }
 }
